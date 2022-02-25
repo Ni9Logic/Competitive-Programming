0546adb056cb90a2a77d9a55ee1f006384be0bcb
ni9logic <rehmanhassan117@gmail.com>
2022-02-25 21:14:27 +0500

    .

--- Google/hashcode.py
+++ Google/hashcode.py
@@ -4,14 +4,12 @@ completed_projects = []
 class Person:
     def __init__(self):
         multiple_input = input().rstrip().split()
-        name = multiple_input[0]
-        self.name = name
-        skills = int(multiple_input[1])
-        self.skills = skills
+        self.name = multiple_input[0]
+        self.skills = int(multiple_input[1])
         self.days_worked = 0
         self.totalskills = []
         self.isBusy = False
-        for i in range(skills):
+        for i in range(self.skills):
             multiple_input = input().rstrip().split()
             self.totalskills.append([multiple_input[0], int(multiple_input[1])])
             
@@ -31,10 +29,24 @@ class Project:
             self.required_skills.append([multiple_input[0], int(multiple_input[1])])
             
             
+            
 Persons = []
 Projects = []
 
-def inputs():
+class Working:
+    def worke():
+        currentday = 0
+        for project in Projects:
+            print(project.name)
+        #! I want to get the project with maximum score first!
+        for i in range(len(Projects)):
+            for j in range(i+1, len(Projects)):
+                if Projects[i].days_required > Projects[j].days_required:
+                    Projects[i], Projects[j] = Projects[j], Projects[i]
+                    
+        for project in Projects:
+            print(project.name)
+def Algo():
     multiple_input = input().rstrip().split()
     c = int(multiple_input[0])
     p1 = int(multiple_input[1])
@@ -45,10 +57,12 @@ def inputs():
     for i in range(p1):
         pp = Project()
         Projects.append(pp)
-        
+
+    Working.worke()
         
 def main():
-   inputs()
+   Algo()
+   
   
         
 main()        
