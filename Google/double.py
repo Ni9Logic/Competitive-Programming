5debfc6cf295ba9aa7c9e8b211c3d39a60b9cd5b
ni9logic <rehmanhassan117@gmail.com>
2022-04-09 10:46:05 +0500

    update

--- Google/double.py
+++ Google/double.py
@@ -1,9 +1,13 @@
+def insert_in_string(string, index, string2):
+    return string[:index] + string2 + string[index:]
+
 def solve():
-    lists = list(map(str, input().rstrip()))
-    for char in lists:
-       print(lists.count(char))
-    
-    print(lists)
+    lists = input()
+    for i in range(len(lists)):
+        if lists.count(lists[i]) % 2 == 0:
+            newlists = insert_in_string(lists, lists.index(lists[i]), lists[i])
+            
+    print(newlists)
 
 def main():
     for i in range(int(input())):
