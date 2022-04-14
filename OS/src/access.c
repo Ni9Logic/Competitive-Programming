17f69e73247270299f12f4e556e6962e84357bc8
ni9logic <rehmanhassan117@gmail.com>
2022-04-14 09:09:25 +0500

    new files

---
+++ OS/src/access.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+extern int errno;
+
+int main()
+{
+    int fd = access("text/myfile.txt", F_OK);
+    if (fd == -1)
+    {
+        printf("Error Number: %d\n", errno);
+        perror("Error Description: ");
+    }
+    else
+        printf("No error\n");
+
+    return 0;
+}
--- OS/src/stat.c
+++ OS/src/stat.c
@@ -38,29 +38,28 @@ void sfile(char const filename[])
 {
     struct stat sfile;
     if (stat(filename, &sfile) == -1)
-    {
         printf("Error Occurred\n");
-    }
+
+
     // Accessing data members of stat struct
-    printf("\nFile st_uid %d \n", sfile.st_uid);
-    printf("\nFile st_blksize %ld \n", sfile.st_blksize);
-    printf("\nFile st_gid %d \n", sfile.st_gid);
-    printf("\nFile st_blocks %ld \n", sfile.st_blocks);
-    printf("\nFile st_size %ld \n", sfile.st_size);
-    printf("\nFile st_nlink %u \n", (unsigned int)sfile.st_nlink);
-    printf("\nFile Permissions User\n");
+    printf("File st_uid %d", sfile.st_uid);
+    printf("\nFile st_blksize %ld", sfile.st_blksize);
+    printf("\nFile st_gid %d", sfile.st_gid);
+    printf("\nFile st_blocks %ld", sfile.st_blocks);
+    printf("\nFile st_size %ld", sfile.st_size);
+    printf("\nFile st_nlink %u", (unsigned int)sfile.st_nlink);
+    printf("\nFile Permissions User ");
     printf((sfile.st_mode & S_IRUSR) ? "r" : "-");
     printf((sfile.st_mode & S_IWUSR) ? "w" : "-");
     printf((sfile.st_mode & S_IXUSR) ? "x" : "-");
-    printf("\n");
-    printf("\nFile Permissions Group\n");
+    printf("\nFile Permissions Group ");
     printf((sfile.st_mode & S_IRGRP) ? "r" : "-");
     printf((sfile.st_mode & S_IWGRP) ? "w" : "-");
     printf((sfile.st_mode & S_IXGRP) ? "x" : "-");
-    printf("\n");
-    printf("\nFile Permissions Other\n");
+    printf("\nFile Permissions Other ");
     printf((sfile.st_mode & S_IROTH) ? "r" : "-");
     printf((sfile.st_mode & S_IWOTH) ? "w" : "-");
     printf((sfile.st_mode & S_IXOTH) ? "x" : "-");
     printf("\n");
+    
 }
