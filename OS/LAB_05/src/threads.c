9c9602af01da5d399c48181d1438268eac9da77c
ni9logic <rehmanhassan117@gmail.com>
2022-04-21 08:20:31 +0500

    .

---
+++ OS/LAB_05/src/threads.c
@@ -0,0 +1,19 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void *myThread(void *vargp)
+{
+    sleep(1);
+    printf("Process thread is being created\n");
+}
+int main()
+{
+    pthread_t thread_1;
+    printf("Before creation of the thread\n");
+    pthread_create(&thread_1, NULL, myThread, NULL);
+    pthread_join(thread_1, NULL);
+    printf("After creation of the thread\n");
+    exit(0);
+}
