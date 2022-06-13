892fc30b942039d4d3498dc56635ed32a14d7e91
ni9logic <rehmanhassan117@gmail.com>
2022-06-13 17:54:15 +0500

    .

--- Vspark/moiz.py
+++ Vspark/moiz.py
@@ -1,22 +1,17 @@
 from itertools import combinations_with_replacement as cr
 
-Big_List = []
-
 def main():
+    counter = 0
     List = list(range(1,2000))
        
-    for p in range(2, 2000):
-        combined = list(cr(List, p))
-        for k in combined:
-            if sum(k) % 5 == 0:
-                print(k)
-                Big_List.append(k)
-                
-                
+    combined = list(cr(List, 2))
+    for k in combined:
+        if sum(k) % 5 == 0:
+            counter += 1
         combined = list()
             
-    print(Big_List)
-    print(f"Total subsets of subsets from range(1 - 2000) that makes the sum of 5 = {len(Big_List)}")
-    print(f"Total number of subsets with combinations of those subsets as-well are: 51")
+            
+    print(counter)
+   
    
 main()
