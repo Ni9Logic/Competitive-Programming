cdc808b34ea028cdda7cd05be26fb5ea5ed821d6
ni9logic <rehmanhassan117@gmail.com>
2022-06-13 17:14:04 +0500

    .

--- Vspark/moiz.py
+++ Vspark/moiz.py
@@ -3,17 +3,17 @@ from itertools import combinations_with_replacement as cr
 Big_List = []
 
 def main():
-    List = []
-    for i in range(1,2000): #Just for the sake of question!
-        if i > 6: #List is greater than 5 is literally useless and unnecessary logic wise in coding context
-            break
-        List.append(i) #List = [1, 2, 3, 4, 5] If every single combination's sum == 5 or not.
+    List = list(range(1,2000))
        
-    for p in range(2, 6):
+    for p in range(2, 2000):
         combined = list(cr(List, p))
         for k in combined:
-            if sum(k) == 5:
+            if sum(k) % 5 == 0:
+                print(k)
                 Big_List.append(k)
+                
+                
+        combined = list()
             
     print(Big_List)
     print(f"Total subsets of subsets from range(1 - 2000) that makes the sum of 5 = {len(Big_List)}")
