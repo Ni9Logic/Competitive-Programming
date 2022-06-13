17aad3cc72b2655eee9cc2fb22b034605c71188a
ni9logic <rehmanhassan117@gmail.com>
2022-06-13 17:54:29 +0500

    .

--- Vspark/moiz.py
+++ Vspark/moiz.py
@@ -3,14 +3,14 @@ from itertools import combinations_with_replacement as cr
 def main():
     counter = 0
     List = list(range(1,2000))
-       
-    combined = list(cr(List, 2))
-    for k in combined:
-        if sum(k) % 5 == 0:
-            counter += 1
-        combined = list()
-            
-            
+    for p in range(2, 2000):
+        combined = list(cr(List, 2))
+        for k in combined:
+            if sum(k) % 5 == 0:
+                counter += 1
+            combined = list()
+                
+                
     print(counter)
    
    
